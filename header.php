fea0f67a3922c496b3577c7d2379fa159dd0fef7
BP <bp@BPs-MacBook-Air.local>
2026-03-25 16:48:24 +0530

    gsc code added

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 
+<meta name="google-site-verification" content="DOxjIINx-dZyHBC1RmBU6_lJA_qIsp4dlTEZ09llrxk" />
 <link rel="icon" href="assets/img/SBI-4_11zon.webp" sizes="35x35" type="image/png">
         
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
